ae4096de2ce2a41385fbf0e0539c2729cb1fe745
Austin Lingenfelter <austin.lingenfelter@gmail.com>
2023-12-02 23:36:49 -0600

    working solutions for day 2

---
+++ day-02/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"log"
+	"strings"
+	"regexp"
+	"strconv"
+	"unicode"
+)
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func checkComma(word string) (bool){
+	for i := 0; i < len(word); i++ {
+		if string(word[i]) == ","{
+			return true
+		}
+	}
+	return false
+}
+
+func gameValues(gameSplit []string)(int, int, int){
+	red := regexp.MustCompile(`red.?`)
+	blue := regexp.MustCompile(`blue.?`)
+	green := regexp.MustCompile(`green.?`)
+	var redPulled int
+	var bluePulled int
+	var greenPulled int
+	for i := range gameSplit {
+			var redNum int
+			var blueNum int
+			var greenNum int
+			var stringValue string
+			spacePull := gameSplit[i]
+		    pull := strings.ReplaceAll(spacePull, " ", "")
+			for _, char := range pull {
+				if unicode.IsDigit(char) {
+					stringValue += string(char)
+				}
+			}
+			if red.Match([]byte(pull)) {
+				numValue, err := strconv.Atoi(string(stringValue))
+				if err != nil {
+						panic(err)
+				}
+				redNum = numValue
+			} else if  blue.Match([]byte(pull)){
+				numValue, err := strconv.Atoi(string(stringValue))
+				if err != nil {
+						panic(err)
+				}
+				blueNum = numValue
+			}else if green.Match([]byte(pull)){
+				numValue, err := strconv.Atoi(string(stringValue))
+				if err != nil {
+						panic(err)
+				}
+				greenNum = numValue
+			}
+			redPulled += redNum
+			bluePulled += blueNum
+			greenPulled += greenNum
+		}
+	return redPulled, bluePulled, greenPulled
+}
+
+func main() {
+	
+	redPossible := 12
+	greenPossible := 13
+	bluePossible := 14
+	var totalPossible int
+
+	lines, err := readLines("input.txt")
+	if err != nil {
+		log.Fatalf("readLines: %s", err)
+	}
+
+	for line := range lines {
+		redPulled := 0
+		greenPulled := 0
+		bluePulled := 0
+
+		gameNum := strings.SplitAfter(lines[line], ":")[0]
+		game := strings.SplitAfter(lines[line], ":")[1]
+		fmt.Printf("Game Num is %s\n",gameNum)
+		colonSplit := strings.Split(game, ";")
+		fmt.Println(colonSplit)
+		for i := range colonSplit {
+			fmt.Println(colonSplit[i])
+			appendGame := strings.Split(colonSplit[i], ",")
+			fmt.Println(appendGame)
+			redPulled, bluePulled, greenPulled = gameValues(appendGame)
+			fmt.Println(redPulled, " ", redPossible)
+			fmt.Println(bluePulled)
+			fmt.Println(greenPulled)
+
+			if redPulled < redPossible || bluePulled < bluePossible || greenPulled < greenPossible {
+			 	fmt.Printf("Game %d is not possible\n", line+1)
+				totalPossible += (line+1)
+			}
+		}
+	}
+	 fmt.Printf("Total possible is: %d\n", totalPossible)
+}
